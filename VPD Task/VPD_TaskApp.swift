1d2836bd3924123d176589ef7e0f0efbcf87226d
Inyene Etoedia <etoedia@gmail.com>
2025-01-08 23:33:23 -0800

    Initial Commit

---
+++ VPD Task/VPD_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPD_TaskApp.swift
+//  VPD Task
+//
+//  Created by Inyene on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct VPD_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
